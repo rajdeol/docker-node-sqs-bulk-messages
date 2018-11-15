526b6fd806da9a517b4dbdcf8bf2be7f3ea03aba
Rajinder Pal Singh <RajinderPal.Singh@team.telstra.com>
2018-11-15 14:58:31 +1100

    Updated to process multiple messages

--- index.js
+++ index.js
@@ -14,38 +14,73 @@ const logger = require(process.env.APP_SERVICES_DIR + 'logger');
 // initiate Handler
 const handleMessage = require(process.env.APP_SERVICES_DIR + 'handler');
 
-//initiate sqs-consumer
-const app = Consumer.create({
-  sqs: sqs,
-  queueUrl: process.env.SQS_QUEUE_URL,
-  visibilityTimeout: process.env.SQS_VISIBILITY_TIMEOUT,
-  terminateVisibilityTimeout: true,
-  attributeNames: ['All'],
-  handleMessage
-});
+// consumer array
+var consumers = [];
 
-// attach event handlers to the sqs-consumer to log messages and track if there is any error
-app.on('error', err => {
-  logger.error('Error in Team data aggregator Consumer : ' + err.message);
-  // stop the polling
-  app.stop();
-});
+// default consumer instance to run
+var number_of_consumers = 1;
 
-app.on('message_received', message => {
-  logger.info('message received from queue : ' + message.Body);
-});
+// check if we have to create multiple consumers
+if (process.env.NUMBER_OF_CONSUMERS) {
+  number_of_consumers = process.env.NUMBER_OF_CONSUMERS;
+}
 
-app.on('message_processed', message => {
-  logger.info('message processed : ' + message.Body);
-});
+// Create five consumers
+for (i = 0; i < number_of_consumers; i++) {
+  consumers[i] = createConsumer(i);
+}
 
-app.on('empty', () => {
-  logger.info('Queue is Empty');
-});
+function createConsumer(i) {
+  //initiate sqs-consumer
+  const app = Consumer.create({
+    sqs: sqs,
+    queueUrl: process.env.SQS_QUEUE_URL,
+    visibilityTimeout: process.env.SQS_VISIBILITY_TIMEOUT,
+    terminateVisibilityTimeout: true,
+    attributeNames: ['All'],
+    batchSize: 10,
+    handleMessage
+  });
 
-app.on('stopped', () => {
-  logger.info('Consumer has stopped');
-  process.exit(1);
-});
-logger.info('Starting the consumer');
-app.start();
+  // attach event handlers to the sqs-consumer to log messages and track if there is any error
+  app.on('error', err => {
+    logger.error('Consumer - ' + i + ': Error in Consumer : ' + err.message);
+    // stop the polling
+    app.stop();
+  });
+
+  app.on('message_received', message => {
+    // logger.info(
+    //   'Consumer - ' + i + ':message received from queue : ' + message.Body
+    // );
+  });
+
+  app.on('message_processed', message => {
+    logger.info('Consumer - ' + i + ':message processed : ' + message.Body);
+  });
+
+  app.on('response_processed', () => {
+    logger.info('Consumer - ' + i + ':Batch of messages processed');
+  });
+
+  app.on('empty', () => {
+    logger.info('Consumer - ' + i + ':Queue is Empty');
+  });
+
+  app.on('stopped', () => {
+    logger.info('Consumer - ' + i + ':Consumer has stopped');
+    // delete the consumer if it has stopped
+    delete consumers[i];
+    // check if all consumers are deleted then exit
+    for (key in consumers) {
+      if (key != 'undefined') {
+        return;
+      }
+    }
+    process.exit(1);
+  });
+  logger.info('Starting the consumer - ' + i);
+  app.start();
+
+  return app;
+}
--- write_message.js
+++ write_message.js
@@ -8,26 +8,30 @@ const sqs = require(process.env.APP_SERVICES_DIR + 'sqs')(
   process.env.SQS_REGION,
   process.env.SQS_API_VERSION
 );
+// Testing Bulk messages
 
-// Prepare a default test message, this message will be pushed if no message is supplied as an argument when calling this file
-let messageBody = 'Test Message created at - ' + new Date().toISOString();
+for (i = 0; i < 50; i++) {
+  // Prepare a default test message, this message will be pushed if no message is supplied as an argument when calling this file
+  let messageBody =
+    'Test Message TEST-' + i + ' created at - ' + new Date().toISOString();
 
-// if there is any message supplied in command line when calling this file, overwrite the test message
-if (process.argv[2]) {
-  messageBody = process.argv[2];
-}
-
-// SQS params
-let sqsParams = {
-  QueueUrl: process.env.SQS_QUEUE_URL,
-  MessageBody: messageBody
-};
-
-// finally push the message to SQS
-sqs.sendMessage(sqsParams, function(err, data) {
-  if (err) {
-    logger.error('Error while writing message to queue', err);
-  } else {
-    logger.info('Message written Successfully to queue : ' + data.MessageId);
+  // if there is any message supplied in command line when calling this file, overwrite the test message
+  if (process.argv[2]) {
+    messageBody = process.argv[2];
   }
-});
+
+  // SQS params
+  let sqsParams = {
+    QueueUrl: process.env.SQS_QUEUE_URL,
+    MessageBody: messageBody
+  };
+
+  // finally push the message to SQS
+  sqs.sendMessage(sqsParams, function(err, data) {
+    if (err) {
+      logger.error('Error while writing message to queue', err);
+    } else {
+      logger.info('Message written Successfully to queue : ' + data.MessageId);
+    }
+  });
+}
